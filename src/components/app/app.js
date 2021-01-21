3042d802b4470a1c4d749f061eff483c84437a7d
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-01-21 21:05:00 +0300

    add checkbox component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="app">
       <Input name="Label" placeholder="placeholder" errorMsg="Validation error message" />
-      <Input name="Label" placeholder="placeholder" errorMsg="Validation error message" />
     </div>
   );
 }

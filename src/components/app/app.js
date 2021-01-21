fec977786e15f05b92c964046f4307fee129f545
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-01-21 21:16:19 +0300

    add textarea component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,12 @@
 import './app.scss';
 import Input from '../input/input';
+import Checkbox from '../checkbox/checkbox';
 
 function App() {
   return (
     <div className="app">
       <Input name="Label" placeholder="placeholder" errorMsg="Validation error message" />
+      <Checkbox />
     </div>
   );
 }
---
+++ src/components/checkbox/checkbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './checkbox.scss';
+
+const Checkbox = () => {
+  return (
+    <label className="checkbox">
+      <input className="checkbox__item" type="checkbox" />
+    </label>
+  );
+};
+
+export default Checkbox;

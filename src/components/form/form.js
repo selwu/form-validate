3f09958423cd8395bc8b6a8ff7338fbecb2ecfba
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-01-22 23:13:08 +0300

    add form styles

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -9,24 +9,28 @@ import Button from '../button/button';
 const Form = () => {
   return (
     <>
-      <form action="">
-        <div>
-          <h2>Регистрация</h2>
-          <p>
-            Уже есть аккаунт?
-            <Link>Войти</Link>
-          </p>
-          <Input />
-          <Input />
-          <Input />
-          <Dropdown />
-          <label>
-            <Checkbox />
-            Принимаю
-            <a target="_blank">условия</a>
-            использования
-          </label>
-        </div>
+      <form action="" className="form">
+        <h2 className="form__title">Регистрация</h2>
+        <p className="form__text">
+          Уже есть аккаунт?&nbsp;&nbsp;
+          <Link to="/" className="form__title-link">
+            Войти
+          </Link>
+        </p>
+        <Input name="Имя" placeholder="Введите Ваше имя" />
+        <Input name="Еmail" placeholder="Введите ваш email" />
+        <Input name="Номер телефона" placeholder="Введите номер телефона" />
+        <Dropdown />
+        <label className="form__checkbox">
+          <Checkbox />
+          <span className="form__contract">
+            Принимаю&nbsp;
+            <a href="#" target="_blank" className="form__checkbox-link">
+              условия
+            </a>
+            &nbsp;использования
+          </span>
+        </label>
         <Button name="Зарегистрироваться" />
       </form>
     </>

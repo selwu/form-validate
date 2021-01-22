64cdd704eea8f6e89df6381bde6e382ec029e741
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-01-22 21:31:25 +0300

    add form structure

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -4,6 +4,7 @@ import './form.scss';
 import Input from '../input/input';
 import Dropdown from '../dropdown/dropdown';
 import Checkbox from '../checkbox/checkbox';
+import Button from '../button/button';
 
 const Form = () => {
   return (
@@ -26,6 +27,7 @@ const Form = () => {
             использования
           </label>
         </div>
+        <Button name="Зарегистрироваться" />
       </form>
     </>
   );

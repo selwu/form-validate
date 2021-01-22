235dbbf00630d97288a97075cb1fd0d6f93493d4
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-01-22 21:02:02 +0300

    dropdown fix opacity

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -16,58 +16,56 @@ const Dropdown = () => {
   return (
     <div className="dropdown">
       <h2 className="dropdown__title">Язык</h2>
+
+      <div className={isActive ? 'dropdown__container active' : 'dropdown__container'}>
+        <label className="dropdown__label">
+          <input
+            onChange={inputHandler}
+            checked={inputHandler}
+            value={'ru'}
+            name="category"
+            className="dropdown__radio"
+            type="radio"
+          />
+          Русский
+        </label>
+        <label className="dropdown__label">
+          <input
+            onChange={inputHandler}
+            checked={inputHandler}
+            value={'en'}
+            name="category"
+            className="dropdown__radio"
+            type="radio"
+          />
+          Английский
+        </label>
+        <label className="dropdown__label">
+          <input
+            onChange={inputHandler}
+            checked={inputHandler}
+            value={'ch'}
+            name="category"
+            className="dropdown__radio"
+            type="radio"
+          />
+          Китайский
+        </label>
+        <label className="dropdown__label">
+          <input
+            onChange={inputHandler}
+            checked={inputHandler}
+            value={'is'}
+            name="category"
+            className="dropdown__radio"
+            type="radio"
+          />
+          Испаниский
+        </label>
+      </div>
       <div onClick={dropdownHandler} className="dropdown__selected">
         Язык
       </div>
-
-      {isActive && (
-        <div className="dropdown__container">
-          <label className="dropdown__label">
-            <input
-              onChange={inputHandler}
-              checked={inputHandler}
-              value={'ru'}
-              name="category"
-              className="dropdown__radio"
-              type="radio"
-            />
-            Русский
-          </label>
-          <label className="dropdown__label">
-            <input
-              onChange={inputHandler}
-              checked={inputHandler}
-              value={'en'}
-              name="category"
-              className="dropdown__radio"
-              type="radio"
-            />
-            Английский
-          </label>
-          <label className="dropdown__label">
-            <input
-              onChange={inputHandler}
-              checked={inputHandler}
-              value={'ch'}
-              name="category"
-              className="dropdown__radio"
-              type="radio"
-            />
-            Китайский
-          </label>
-          <label className="dropdown__label">
-            <input
-              onChange={inputHandler}
-              checked={inputHandler}
-              value={'is'}
-              name="category"
-              className="dropdown__radio"
-              type="radio"
-            />
-            Испаниский
-          </label>
-        </div>
-      )}
     </div>
   );
 };
